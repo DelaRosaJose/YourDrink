5483a1832d1a5bb40b1ada4fc20276c268ba193c
DelaRosa_Jose <josealberto3053@gmail.com>
2024-01-25 12:35:35 -0400

    Add project files.

---
+++ YourDrink/Services/HttpService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YourDrink.Services
+{
+    public class HttpService
+    {
+        HttpClient httpClientcli = new HttpClient();
+        private const string BaseUrl = "www.thecocktaildb.com/api/json/v1/1/";
+
+        public static string SearchCocktailByName(string Name) => $"{BaseUrl}search.php?s={Name}";
+        public static string SearchCocktailByLetter(string Letter) => $"{BaseUrl}search.php?f={Letter}";
+        public static string SearchIngredientByName(string Name) => $"{BaseUrl}search.php?i={Name}";
+        public static string SearchCocktailById(string Id) => $"{BaseUrl}lookup.php?i={Id}";
+        public static string SearchRandomCocktail  => $"{BaseUrl}random.php";
+        public static string SearchCocktailByIngredient(string Ingredient) => $"{BaseUrl}filter.php?i={Ingredient}";
+
+
+        public async Task<string> HttpPetition(string Url)
+        {
+            var Request = await httpClientcli.GetAsync(Url);
+            return await Request.Content.ReadAsStringAsync();
+        }
+    }
+}
